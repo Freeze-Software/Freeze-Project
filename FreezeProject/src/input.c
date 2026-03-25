2df7675974cf1fdfce221f26a0e622031f7e0f6d
Clashnewbme <amoryjamesmonson@icloud.com>
2026-03-25 16:40:06 +0800

    fixed holding glitch bug issue

--- FreezeProject/src/input.c
+++ FreezeProject/src/input.c
@@ -1,7 +1,16 @@
 #include "input.h"
-
 #include "serial.h"
 #include "vga.h"
+
+unsigned char inb(unsigned short port) {
+    unsigned char ret;
+    __asm__ volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+void outb(unsigned short port, unsigned char val) {
+    __asm__ volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+}
 
 char scancode_to_ascii(unsigned char sc) {
     static const char map[128] = {
@@ -9,22 +18,28 @@ char scancode_to_ascii(unsigned char sc) {
         '-', '=', 8,   9,   'q',  'w', 'e', 'r',  't', 'y', 'u', 'i',
         'o', 'p', '[', ']', '\n', 0,   'a', 's',  'd', 'f', 'g', 'h',
         'j', 'k', 'l', ';', '\'', '`', 0,   '\\', 'z', 'x', 'c', 'v',
-        'b', 'n', 'm', ',', '.',  '/', 0,   '*',  0,   ' '};
+        'b', 'n', 'm', ',', '.',  '/', 0,   '*',  0,   ' '
+    };
     if (sc < sizeof(map)) return map[sc];
     return 0;
 }
-// SERIAL input-aware reader
+
 void get_input(char* buffer) {
     int i = 0;
+    static unsigned char last_sc = 0;
+
     while (1) {
         if (serial_available()) {
             char c = serial_getc();
+
             if (c == '\r') c = '\n';
+
             if (c == '\n') {
                 buffer[i] = 0;
                 putc('\n');
                 return;
             }
+
             if (c == 8 || c == 127) {
                 if (i > 0) {
                     i--;
@@ -32,38 +47,43 @@ void get_input(char* buffer) {
                 }
                 continue;
             }
+
             if (i < 127) {
                 buffer[i++] = c;
                 putc(c);
             }
+
             continue;
         }
+        if (!(inb(0x64) & 1)) continue; 
 
         unsigned char sc = inb(0x60);
-        if (sc & 0x80) continue;
+
+        if (sc & 0x80) continue; 
+
+        if (sc == last_sc) continue; 
+        last_sc = sc;
+
         char c = scancode_to_ascii(sc);
         if (!c) continue;
+
         if (c == '\n') {
             buffer[i] = 0;
             putc('\n');
             return;
         }
-        if (c == 8 && i > 0) {
-            i--;
-            erase_last_char();
+
+        if (c == 8) {
+            if (i > 0) {
+                i--;
+                erase_last_char();
+            }
             continue;
         }
+
         if (i < 127) {
             buffer[i++] = c;
             putc(c);
         }
     }
-}
-unsigned char inb(unsigned short port) {
-    unsigned char ret;
-    __asm__ volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
-    return ret;
-}
-void outb(unsigned short port, unsigned char val) {
-    __asm__ volatile("outb %0, %1" : : "a"(val), "Nd"(port));
 }
